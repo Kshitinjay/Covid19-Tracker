f360384d6bbd616be8f0f7504877d7ac661a648d
Kshitinjay <kshitinjay20@gmail.com>
2021-04-16 15:20:02 +0530

    commit

--- covid19tracker/src/components/Cards/Card/index.js
+++ covid19tracker/src/components/Cards/Card/index.js
@@ -8,7 +8,7 @@ import styles from './card.module.css';
 const CardComponent = ({ className, cardTitle, value, lastUpdate, cardSubtitle }) => (
   <Grid item xs={12} md={3} component={Card} className={cx(styles.card, className)}>
     <CardContent>
-      <Typography color="textSecondary" gutterBottom>
+      <Typography color="textSecondary" >
         {cardTitle}
       </Typography>
       <Typography variant="h5" component="h2">
